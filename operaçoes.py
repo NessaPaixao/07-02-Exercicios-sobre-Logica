aaecee0dcfa8a089bbd3b817d2a91cc87d78b237
Vanessa da Paixao <vanessadapaixaobarros1@gmail.com>
2019-08-13 18:02:35 -0300

    Adicionar exercicios das operacoes

---
+++ operaçoes.py
@@ -0,0 +1,19 @@
+valor1 = float(input("Digite um numero: "))
+valor2 = float(input("Digite um segundo numero: "))
+
+def soma (valor1, valor2):
+    return valor1+valor2
+
+def sub (valor1, valor2):
+    return valor1-valor2
+
+def mult (valor1, valor2):
+    return valor1*valor2
+
+def div (valor1, valor2):
+    return valor1/valor2
+
+print(soma(valor1,valor2))
+print(sub(valor1,valor2))
+print(mult(valor1,valor2))
+print(div(valor1,valor2))
