38293b7fcbc3a646786dc929662092ca76af82d0
Vanessa da Paixao <vanessadapaixaobarros1@gmail.com>
2019-08-13 15:56:07 -0300

    Adicionar atividade de calculo de salario

---
+++ funcionario1.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu nome: ")
+horas = float(input("Digite suas horas de trabalho: "))
+valor = float(input("Digite o valor de cada hora: "))
+
+salario =  horas * valor
+print(str(nome) + " seu salário é: " + "R$" + str(salario) )
